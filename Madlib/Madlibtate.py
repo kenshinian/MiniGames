502f74a6baf0750e9cf0f19eb66a7a400c1850f6
Ian Pacquing <155401693+kenshinian@users.noreply.github.com>
2024-01-03 17:27:18 +0800

    Create Madlibtate.py

---
+++ Madlib/Madlibtate.py
@@ -0,0 +1,28 @@
+#ProjectMadlib
+#[01/02/2023]
+def madlib():
+    expression = input("Expression: ")
+    reason = input("Reason: ")
+    noun = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ") 
+    verb =input("Verb: ")
+    ingverb =input("Verb: ")
+    sports=input("Sports: ")
+    adjective=input("Adjective:")
+    adjective2=input("Adjective:")
+    dream=input("Dream:")
+    cardname=input("Cardname: ")
+
+    madlib= f"{expression}, listen up, because Im not here to sugarcoat anything. Life doesnt give a damn about your {reason}.If youre not where you want to be its on you. \
+    There are no handouts  no sympathy prizes. {noun} dont cry about the cards theyre \
+    deals they {verb} them like masters. I was a {sports} world champion not because\
+    I was born {adjective} but because I worked my ass off every single day. \
+    Youre not entitled to success; you earn it through {noun2}, {noun3}, and pure{noun4}.\
+    You think the world owes you something? Think again. It owes you nothing. \
+    Youre not a victim; youre a {dream}. Stop playing the {cardname} card and start {ingverb} to win.\
+    Life hits hard, and it hits {adjective2}."
+
+    print(madlib)
+    
